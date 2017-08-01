4b8a80866e51241571dadd215f1417375ac780c4
rohit <rohit.nandamudi@just-roar.com>
2017-08-01 15:02:26 +0530

    completing map functionality for home

--- src/app/gmtest/gmtest.component.ts
+++ src/app/gmtest/gmtest.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
+import * as config from '../configs/configs';
 
 declare var google: any;
 declare var MarkerClusterer: any;
@@ -13,6 +16,7 @@ declare var $: any;
 export class Gmtest {
     private search = '';
     @Input() item: any;
+    @Input() loc: any;
     @Input() trailers: any = [];
 
     @Output() itemChange = new EventEmitter<any>();
@@ -20,11 +24,17 @@ export class Gmtest {
     markers: any;
     map: any;
     infowindow: any;
-    index:any;
+    index: any;
+    historyRecv:any=false;
     markerList: any = {
         blueMark: '../../assets/images/markers/trailer-blue.png',
         redMark: '../../assets/images/markers/trailer-red.png',
         greenMark: '../../assets/images/markers/trailer-green.png',
+    };
+    selectedMarker: any = { "trailerID": "25002", "trailerType": "UNK", "latitude": 33.86423, "longitude": -81.03682, "location": "Cayce,SC", "landmark": "Cayce", "trailerStatus": "Planned", "idleDuration": 0.0, "lastMovementDate": "UNKNOWN", "dotDate": "UNKNOWN", "iotInfo": "INACTIVE", "compliance": "", "roadWorthiness": "" };
+    history = {
+        column: [{ name: "Date", width: "25%" }, { name: "Order ID", width: "25%" }, { name: "Start City", width: "25%" }, { name: "Destination City", width: "25%" }],
+        groups: [{ "pID": 41, "poolID": "AMAJOL", "cmpID": "AMAJOL", "planner": "COOPER", "csr": "Jacob", "reqPoolCount": 16, "avaiPoolCount": 4, "variance": 12, "stateCode": "IL", "stateName": "Illinois", "companyName": "AMAZON - MDW2", "cityName": "Joliet", "isShipper": "Y", "active": "Y", "isReceiver": "N", "brand": "CVEN" }, { "pID": 42, "poolID": "AMAKEN02", "cmpID": "AMAKEN02", "planner": "WILL", "csr": "Ryan", "reqPoolCount": 15, "avaiPoolCount": 6, "variance": 9, "stateCode": "WI", "stateName": "Wisconsin", "companyName": "AMAZON - MKE1", "cityName": "Kenosha", "isShipper": "Y", "active": "Y", "isReceiver": "Y", "brand": "CVEN" }]
     };
 
     mapIcon(trailer: any) {
@@ -37,6 +47,10 @@ export class Gmtest {
         }
     }
 
+    emit() {
+        this.itemChange.emit(this.item);
+    }
+
     ngOnChanges(changes: any) {
         console.log("parameters changed " + this.trailers.length);
         //this.addTrailers(changes.trailerList.currentValue);
@@ -45,6 +59,7 @@ export class Gmtest {
         }
 
     }
+    constructor(private http: Http) {}
 
     ngOnInit() {
         this.geocoder = new google.maps.Geocoder();
@@ -67,6 +82,12 @@ export class Gmtest {
         this.infowindow = new google.maps.InfoWindow({
             content: ""
         });
+
+        var input = document.getElementById('ctgGeoCode');
+        var autoCompOptions = {
+            componentRestrictions: { country: 'us' }
+        };
+        var autocomplete = new google.maps.places.Autocomplete(input, autoCompOptions);
 
         for (var i = 0; i < this.trailers.length; i++) {
             var tr = this.trailers[i];
@@ -119,6 +140,37 @@ export class Gmtest {
             mcOptions);
     }
 
+    createinfoWinContent(tr: any) {
+        var color = ""; var status = ""
+        if (tr.trailerStatus == 'AVL') {
+            color = "#15c922";
+            status = "AVAILABLE"
+        } else if (tr.trailerStatus == 'PLN') {
+            color = "#158cff";
+            status = "PLANNED";
+        } else {
+            color = "#fb3447";
+            status = "CONFIRMED"
+        }
+        var content = '<div class="infowindow" style="width:200px;padding:0px;height:162px;overflow:hidden;">' +
+            '<div class="row header" style="border-bottom: 2px solid gray;padding:0px 0px 0px 30px">' +
+            '<div class="row head1" style="font-weight:bold;font-size:14px;color:black">Trailer #: ' + tr.trailerID + '</div>' +
+            '<div class="row head2" style="font-weight:bold;font-size:13px;color:red">'+tr.trailerName+'</div>' +
+            '<div class="row head3" style="font-weight:bold;font-size:12px;color:black">' + tr.trailerType + '</div>' +
+            '</div>' +
+            '<div class="row title" style="border-bottom:1px solid silver;height:35px;padding:0px 30px 0px 15px">' +
+            '<span class="vehicle-date" style="font-size:10px;float:left;padding-top:5px"><b>DOT Date:</b><br>' + tr.dotDate + '</span>' +
+            '<span class=" row vehicletype available" style="float:right;font-size:14px;margin-top:4px;font-weight:bold;color:' + color + '">' + status + '</span>' +
+            '</div>' +
+            '<div class="row content" style="padding:3px 30px 0px 15px">' +
+            '<span><b>Location</b></span><br>' + tr.location +
+            '<br><br><span><b>lat: </b>' + tr.latitude + '<b></span><br><span>lng: </b>' + tr.longitude + '</span>' +
+            '</div>' +
+
+            '</div>';
+        return content;
+    }
+
     bindInfoWindow(item, marker, map, infowindow, html) {
         var ob = this;
         google.maps.event.addListener(marker, 'mouseover', function () {
@@ -140,47 +192,50 @@ export class Gmtest {
         });
     }
 
-    createinfoWinContent(tr: any) {
-        var color = ""; var status = ""
-        if (tr.trailerStatus == 'AVL') {
-            color = "#15c922";
-            status = "AVAILABLE"
-        } else if (tr.trailerStatus == 'PLN') {
-            color = "#158cff";
-            status = "PLANNED";
-        } else {
-            color = "#fb3447";
-            status = "CONFIRMED"
-        }
-        var content = '<div class="infowindow" style="width:200px;padding:0px;height:158px;overflow:hidden;">' +
-            '<div class="row header" style="border-bottom: 2px solid gray;padding:0px 0px 0px 30px">' +
-            '<div class="row head1" style="font-weight:bold;font-size:16px:color:black">Trailer #: ' + tr.trailerID + '</div>' +
-            '<div class="row head2" style="font-weight:bold;font-size:14px:color:red">TR43</div>' +
-            '<div class="row head3" style="font-weight:bold;font-size:12px:color:black">' + tr.trailerType + '</div>' +
-            '</div>' +
-            '<div class="row title" style="border-bottom:1px solid silver;height:30px;padding:0px 30px 0px 15px">' +
-            '<span class="vehicle-date" style="font-size:10px;float:left;padding-top:5px">DOT Date:' + tr.dotDate + '</span>' +
-            '<span class=" row vehicletype available" style="float:right;font-size:14px;margin-top:4px;font-weight:bold;color:' + color + '">' + status + '</span>' +
-            '</div>' +
-            '<div class="row content" style="padding:0px 30px 0px 15px">' +
-            '<span><b>Location</b></span><br>' + tr.location +
-            '<br><br><span><b>lat: </b>' + tr.latitude + '<b></span><br><span>lng: </b>' + tr.longitude + '</span>' +
-            '</div>' +
-
-            '</div>';
-        return content;
-    }
-
     selectMarker(item) {
 
-        this.item=item;
-        this.itemChange.emit(this.item);
+        this.selectedMarker = item;
+        this.getTrailerHistory();
+        $('#gmHistoryModal').modal('show');
+
+        //this.selectedMarkerChange.emit(this.selectedMarker);
 
         //this.increment(item);
     }
 
+    geocodeAddress(addr: any) {
+        //this.state = addr;
+        //alert(this.state);
+        var ctrl = this;
+        var geocoder = this.geocoder;
+        var resultsMap = this.map;
+        var address = addr;
+        //var bound: any = this.boundList;
+
+
+        geocoder.geocode({ 'address': address }, function (results: any, status: any) {
+            if (status === 'OK') {
+                var markerBounds = new google.maps.LatLngBounds();
+                resultsMap.setCenter(results[0].geometry.location);
+                markerBounds.extend(results[0].geometry.location);
+                let state = (results[0].address_components[0].long_name);
+                let code = (results[0].address_components[0].short_name);
+                let bounds = (results[0].geometry.bounds);
+                let location = results[0].geometry.location;
+                //bound.push({ state: state, bounds: bounds, code: code, location: location });
+                resultsMap.fitBounds(results[0].geometry.viewport);
+                resultsMap.setZoom(10);
+                this.map = resultsMap;
+                //ctrl.test(results[0].geometry.bounds);
+            } else {
+                alert('Geocode was not successful for the following reason: ' + status);
+
+            }
+        });
+
+    }
+
     call(item: any) {
-        var obTemp = this;
         for (var i = 0; i < this.trailers.length; i++) {
             var obj = this.trailers[i];
             if (obj.trailerID == item) {
@@ -188,7 +243,7 @@ export class Gmtest {
                 var pos = new google.maps.LatLng(obj.latitude, obj.longitude)
                 this.map.setCenter(pos);
                 this.map.setZoom(25);
-                
+                var obTemp = this;
                 //this.infowindow.setContent(this.createinfoWinContent(obj));
                 //this.infowindow.open(this.map, obTemp.markers[i]);
                 this.index = i;
@@ -205,6 +260,24 @@ export class Gmtest {
             google.maps.event.trigger(obTemp.markers[obTemp.index], 'mouseover');
         }, 500);
     }
+
+    getTrailerHistory() {
+        this.historyRecv = false;
+        let url = config.baseUrl + "/HomeService/api/TrailerHistory?trailerID=" + this.selectedMarker.trailerID;
+        this.http.get(url).map(res => res.json())
+            .subscribe(
+            (data) => {
+                console.log("StatesTrailerCounts data recieved");
+                this.history.groups = data;
+                this.historyRecv = true;
+            }, //For Success Response
+            (err) => {
+                console.log("StatesTrailerCounts error recieved");
+                this.historyRecv = true;
+            } //For Error Response
+            );
+    }
+
 
 }
 
