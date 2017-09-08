95982b5c089cec141839d6430d0065536fa97eae
rohit <rohit.nandamudi@just-roar.com>
2017-09-08 13:32:07 +0530

    adding order dashboard component

---
+++ src/app/order-dash/order-dash.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-dash',
+  templateUrl: './order-dash.component.html',
+  styleUrls: ['./order-dash.component.css']
+})
+export class OrderDashComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
