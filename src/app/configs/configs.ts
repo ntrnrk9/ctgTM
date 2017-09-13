fd69d10ed0aab36565a97e55227ecfb17d1d0fef
rohit <rohit.nandamudi@just-roar.com>
2017-09-13 14:37:45 +0530

    adding authentication code for flin api

--- src/app/configs/configs.ts
+++ src/app/configs/configs.ts
@@ -4,3 +4,5 @@ export const sep='/';
 export const version: string="22.2.2";
 export const baseUrl: string="https://ctgtest.com";
 export const ctgApiUrl: string="https://api.drivenanalyticsolutions.com/ctg";
+export const username: string="trailer_management";
+export const password:String="tPCq5!0rbm.qL$KynV8# wEez6*";
