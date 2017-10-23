8daeab9581d5b9f6a5badf2279636fcb1cd7b873
ntrnrk9@gmail.com <ntrnrk9@gmail.com>
2017-10-23 12:58:56 +0530

    devloping login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,143 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, Output, ViewChild, NgZone, ChangeDetectorRef, OnInit, EventEmitter } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { IntervalObservable } from 'rxjs/observable/IntervalObservable';
+import { GmapjsComponent } from '../gmapjs/gmapjs.component';
+import { Gmtest } from '../gmtest/gmtest.component';
+import { IMyDpOptions, IMyDateModel, IMyInputFieldChanged } from 'mydatepicker';
+declare var $: any;
+import * as config from '../configs/configs';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  @Input() config: any;
+  @Output() configChange = new EventEmitter<any>();
+  username = "";
+  password = "";
+  authToken = ""
+  user = "";
+  invalidCred=false;
+  action: any = { heading: "", body: "",details: ""};
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   ngOnInit() {
   }
 
+  emit() {
+    this.configChange.emit(this.config);
+    console.log(this.config);
+  }
+
+  login() {
+    console.log(this.username);
+    console.log(this.password);
+    this.getAuthToken();
+  }
+
+  getAuthToken() {
+
+    var creds = "username=" + config.username + "&password=" + config.password;
+
+    let authToken = "Basic";
+    let headers = new Headers({ 'Accept': 'application/json' });
+    headers.append('Authorization', 'Basic ' +
+      btoa(config.username + ':' + config.password));
+
+    //let options = new RequestOptions({ headers: headers });
+    let options = {
+      headers: headers
+    };
+    let postBody = { "username": this.username, "password": this.password };
+
+    let url = "https://api.drivenanalyticsolutions.com/ctg/auth/token";
+    // let url = config.ctgApiUrl + "/auth/token";
+
+    this.http.post(url, postBody, options).map(res => res.json())
+      .subscribe(
+      (data) => {
+        console.log("getAuthToken data recieved");
+        if (data) {
+          console.log("Login-success: got auth token");
+          console.log(data.Token);
+          console.log(data.ADUser.cn);
+          this.authToken = (data.Token);
+          this.user = (data.ADUser.cn);
+          this.verifyToken();
+
+        } else {
+          console.log("getAuthToken recieved invalid data");
+        }
+
+      }, //For Success Response
+      (err) => {
+        console.log("getAuthToken error recieved");
+        this.invalidCred=true;
+        // var result={};
+        // this.action.heading = "Authentication";
+        // this.action.body = "Unexpected Error occured. Please contact system administrator.";
+        // this.action.details = 'Authentication service failed<br>' + url + '<br>Status: ' + err.status;
+        // $('#unExpecError').modal('show');
+        
+      } //For Error Response
+      );
+  }
+
+
+  verifyToken() {
+
+    var creds = "username=" + config.username + "&password=" + config.password;
+
+    let authToken = "Basic";
+    let headers = new Headers({ 'Accept': 'application/json' });
+    headers.append('Authorization', 'Basic ' +
+      btoa(config.username + ':' + config.password));
+    headers.append("X-Auth-Token", this.authToken);
+    //let options = new RequestOptions({ headers: headers }); 
+    let options = {
+      headers: headers
+    };
+    let postBody = { "username": this.username, "password": this.password };
+
+    let url = "https://api.drivenanalyticsolutions.com/ctg/auth/verify";
+    // let url = config.ctgApiUrl + "/assets/order/assign_trailer";
+
+    this.http.get(url, options).map(res => res.json())
+      .subscribe(
+      (data) => {
+        console.log("allocateInTMW data recieved");
+        if (data) {
+          console.log("TMS Trailers matches with TMW Trailer");
+          if (data.is_valid) {
+            this.config.isValid = true;
+            this.invalidCred=false;
+            this.emit();
+          }else{
+            this.action.heading = "Authentication";
+            this.action.body = "Invalid token. Please login again.";
+            this.action.details = "";
+            $('#unExpecError').modal('show');
+          }
+
+        } else {
+          console.log("TMS Trailers do not match with TMW Trailer");
+          this.action.heading = "Authentication";
+          this.action.body = "Unexpected Error occured. Please contact system administrator.";
+          this.action.details = 'Token validation service recieved invalid data<br>' + url;
+          $('#unExpecError').modal('show');
+        }
+
+      }, //For Success Response
+      (err) => {
+        console.log("allocateInTMW error recieved");
+        this.action.heading = "Authentication";
+        this.action.body = "Unexpected Error occured. Please contact system administrator.";
+        this.action.details = 'Token validation service failed<br>' + url + '<br>Status: ' + err.status;
+        $('#unExpecError').modal('show');
+      } //For Error Response
+      );
+  }
 }
