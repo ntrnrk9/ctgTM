f3a9350ded636c697426a4097f283785f8520b3f
ntrnrk9@gmail.com <ntrnrk9@gmail.com>
2017-11-17 12:09:17 +0530

    adding users with admin privlages

--- src/app/configs/configs.ts
+++ src/app/configs/configs.ts
@@ -2,9 +2,19 @@
 
 export const sep='/';
 export const version: string="22.2.2";
-// export const baseUrl: string="https://ctgtest.com";
-export const baseUrl: string="http://10.4.1.50";
+export const baseUrl: string="https://ctgtest.com";
+// export const baseUrl: string="http://10.4.1.50";
 export const ctgApiUrl: string="https://test.drivenanalyticsolutions.com/ctg";
 // export const ctgApiUrl: string="https://api.drivenanalyticsolutions.com/ctg";
 export const username: string="trailer_management";
 export const password:String="tPCq5!0rbm.qL$KynV8# wEez6*";
+export const adminList = [
+    {
+        id: "subkat"
+    },
+    {
+        id: "rachan"
+    },
+    {
+        id: "kumroh"
+    }];
